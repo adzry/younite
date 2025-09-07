5abb1c1490056ec3896c8d52e1459aeff424342f
adzry <adzrysapie@gmail.com>
2025-09-07 19:08:14 +0800

    chore: clean app.config.js and reset projectId placeholder

--- app.config.js
+++ app.config.js
@@ -1,39 +1,38 @@
 export default {
-  expo: {
-    name: "YOUNITE",
-    slug: "younite",
-    owner: "adzry",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "automatic",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
+  name: "YOUNITE",
+  slug: "younite",
+  owner: "adzry",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.adzry.younite"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#ffffff"
     },
-    updates: {
-      fallbackToCacheTimeout: 0
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.adzry.younite"
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      },
-      package: "com.adzry.younite"
-    },
-    web: {
-      favicon: "./assets/favicon.png"
-    },
-    extra: {
-      eas: {
-        projectId: "00000000-0000-0000-0000-000000000000"
-      }
+    package: "com.adzry.younite"
+  },
+  web: {
+    favicon: "./assets/favicon.png"
+  },
+  extra: {
+    eas: {
+      // 🚀 ProjectId will be injected by `eas init`
+      projectId: null
     }
   }
 };
